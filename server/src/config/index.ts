7ab9df25ef4b3f7fbc521e2eafa9ad3e8b48ff6b
Lukáš Vaněk <lukas@sinesquare.com>
2020-06-04 21:08:21 +0200

    port

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -4,6 +4,6 @@ dotenv.config();
 
 export default {
   db: 'mongodb://127.0.0.1:27017/alpha',
-  port: process.env.PORT,
-  allowedOrigins: ['http://localhost:4000', 'http://localhost:3000']
+  port: 80,
+  allowedOrigins: ['http://localhost:4000', 'http://localhost:3000', 'http://localhost:80']
 };
